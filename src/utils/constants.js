eeab2bb7c5969a46b6d27b12481fd0bf4fe2db05
billtai26 <anhtaipxuan@gmail.com>
2026-01-10 21:21:47 +0700

    Update production API root URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -10,7 +10,7 @@ if (process.env.BUILD_MODE === 'dev') {
 // Môi trường Production sẽ cần api endpoint chuẩn của các bạn
 if (process.env.BUILD_MODE === 'production') {
   // Lưu ý: Đây là domain ví dụ sau khi Deploy Production (xem video 75 và video 76 để hiểu rõ kiến thức phần này, còn hiện tại mình đã xóa domain này rồi, đừng cố truy cập làm gì =))
-  apiRoot = 'https://trello-api-0gbu.onrender.com'
+  apiRoot = 'https://website-quan-ly-cong-viec-ca-nhan.onrender.com'
 }
 // console.log('🚀 ~ file: constants.js:7 ~ apiRoot:', apiRoot)
 export const API_ROOT = apiRoot
